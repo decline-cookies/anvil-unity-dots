4e5884106fdb88b81aec0fc8b762780022256d8b
Mike Baker <1426795+mbaker3@users.noreply.github.com>
2022-03-03 23:35:59 -0500

    Create AbstractValueAccessController
    - Manage access to arbitrary values that are not stored in ECS's chunk system (Ex: Values hosted on systems).
    - Provides and maintains job handles to manage safe job access to the value
    
    Create concrete implementation for NativeArrays (most common use case)

---
+++ Scripts/Runtime/Jobs/ValueAccessController/NativeArrayAccessController.cs
@@ -0,0 +1,14 @@
+using Unity.Collections;
+using Unity.Entities;
+
+namespace Anvil.Unity.DOTS.Jobs
+{
+    /// <summary>
+    /// A wrapper for NativeArray that provides a way to manage async access to the array.
+    /// </summary>
+    public class NativeArrayAccessController<T> : AbstractValueAccessController<NativeArray<T>> where T : struct, IComponentData
+    {
+        public NativeArrayAccessController(int length, NativeArrayOptions options)
+            : base(new NativeArray<T>(length, Allocator.Persistent, options)) { }
+    }
+}
