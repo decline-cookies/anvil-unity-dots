5684c7c672663c65e59c02c7d69f36f2d8746275
Mike Baker <1426795+mbaker3@users.noreply.github.com>
2022-03-04 16:37:53 -0500

    NativeArrayAccessController - Remove IComponentData restriction

--- Scripts/Runtime/Jobs/ValueAccessController/NativeArrayAccessController.cs
+++ Scripts/Runtime/Jobs/ValueAccessController/NativeArrayAccessController.cs
@@ -6,7 +6,7 @@ namespace Anvil.Unity.DOTS.Jobs
     /// <summary>
     /// A wrapper for NativeArray that provides a way to manage async access to the array.
     /// </summary>
-    public class NativeArrayAccessController<T> : AbstractValueAccessController<NativeArray<T>> where T : struct, IComponentData
+    public class NativeArrayAccessController<T> : AbstractValueAccessController<NativeArray<T>> where T : struct
     {
         public NativeArrayAccessController(int length, NativeArrayOptions options)
             : base(new NativeArray<T>(length, Allocator.Persistent, options)) { }
