fadb224691bb8af7fa555ccbd83d1230bb5dddef
Mike Baker <1426795+mbaker3@users.noreply.github.com>
2022-05-17 20:27:29 -0400

    Logging - Add docs

--- Scripts/Runtime/Logging/BurstableLogger.cs
+++ Scripts/Runtime/Logging/BurstableLogger.cs
@@ -5,11 +5,44 @@ using Anvil.Unity.Logging;
 
 namespace Anvil.Unity.DOTS.Logging
 {
+    /// <summary>
+    /// A context specific instance that provides a mechanism to emit logs with minimum boilerplate.
+    /// Automatically decorates log messages with contextual information including:
+    ///  - Optional, per instance, message prefix
+    /// </summary>
+    /// <typeparam name="PrefixStringType">
+    /// The FixedString type to use for the message prefix
+    /// Select the shortest fixed string that will fit the prefix length.
+    /// (usually <see cref="FixedString32Bytes" />)
+    /// </typeparam>
+    /// <remarks>
+    /// In the future, the goal of this type is to provide all the same information that 
+    /// <see cref="Log.Logger" /> provides. That is not possible with Burst today but should be 
+    /// possible in the future without making any changes at the consuming end. At the moment, the 
+    /// contextual information is added when the message passes through <see cref="UnityLogListener"/>.
+    /// This information will appear in the output of all active <see cref="ILogHandler" /> implementations.
+    /// except for the Editor console (<see cref="UnityLogHandler"/>).
+    /// This is a Burst limitation.
+    /// </remarks>
     [BurstCompatible]
     public readonly struct BurstableLogger<PrefixStringType> where PrefixStringType : struct, INativeList<byte>, IUTF8Bytes
     {
+        /// <summary>
+        /// The custom prefix to prepend to all messages sent through this instance.
+        /// </summary>
         public readonly PrefixStringType MessagePrefix;
 
+        /// <summary>
+        /// Creates an instance of <see cref="BurstableLogger{PrefixStringType}"/> from a 
+        /// <see cref="Log.Logger" /> instance.
+        /// </summary>
+        /// <param name="logger">The <see cref="Log.Logger" /> to copy configuration from.</param>
+        /// <param name="appendToMessagePrefix">
+        /// A string to append to the <see cref="Log.Logger" />'s existing prefix.
+        /// </param>
+        /// <exception cref="Exception">
+        /// Thrown if there is an unknown error encountered when configuring the prefix string.
+        /// </exception>
         [NotBurstCompatible]
         public BurstableLogger(Log.Logger logger, string appendToMessagePrefix)
         {
@@ -31,11 +64,21 @@ namespace Anvil.Unity.DOTS.Logging
             }
         }
 
+        /// <summary>
+        /// Creates an instance of <see cref="BurstableLogger{PrefixStringType}"/>.
+        /// </summary>
+        /// <param name="messagePrefix">The custom prefix to prepend to all messages sent through this instance.</param>
         public BurstableLogger(in PrefixStringType messagePrefix)
         {
             MessagePrefix = messagePrefix;
         }
 
+        /// <summary>
+        /// Logs a message.
+        /// </summary>
+        /// <param name="message">
+        /// The message to log. (max length: <see cref="FixedString64Bytes.Capacity"/>)
+        /// </param>
         [UnityLogListener.Exclude]
         public void Debug(in FixedString64Bytes message)
         {
@@ -43,6 +86,16 @@ namespace Anvil.Unity.DOTS.Logging
             UnityEngine.Debug.Log($"{MessagePrefix}{message}");
         }
 
+        /// <summary>
+        /// Logs a message.
+        /// </summary>
+        /// <typeparam name="T">
+        /// The FixedString type for the message
+        /// Select the shortest fixed string that will fit the message length.
+        /// </typeparam>
+        /// <param name="message">
+        /// The message to log. (max length: <see cref="T.Capacity"/>)
+        /// </param>
         [UnityLogListener.Exclude]
         public void Debug<T>(in T message) where T : struct, INativeList<byte>, IUTF8Bytes
         {
@@ -50,6 +103,12 @@ namespace Anvil.Unity.DOTS.Logging
             UnityEngine.Debug.Log($"{MessagePrefix}{message}");
         }
 
+        /// <summary>
+        /// Logs a warning message.
+        /// </summary>
+        /// <param name="message">
+        /// The message to log. (max length: <see cref="FixedString64Bytes.Capacity"/>)
+        /// </param>
         [UnityLogListener.Exclude]
         public void Warning(in FixedString64Bytes message)
         {
@@ -57,6 +116,16 @@ namespace Anvil.Unity.DOTS.Logging
             UnityEngine.Debug.LogWarning($"{MessagePrefix}{message}");
         }
 
+        /// <summary>
+        /// Logs a warning message.
+        /// </summary>
+        /// <typeparam name="T">
+        /// The FixedString type for the message
+        /// Select the shortest fixed string that will fit the message length.
+        /// </typeparam>
+        /// <param name="message">
+        /// The message to log. (max length: <see cref="T.Capacity"/>)
+        /// </param>
         [UnityLogListener.Exclude]
         public void Warning<T>(in T message) where T : struct, INativeList<byte>, IUTF8Bytes
         {
@@ -64,6 +133,12 @@ namespace Anvil.Unity.DOTS.Logging
             UnityEngine.Debug.LogWarning($"{MessagePrefix}{message}");
         }
 
+        /// <summary>
+        /// Logs an error message.
+        /// </summary>
+        /// <param name="message">
+        /// The message to log. (max length: <see cref="FixedString64Bytes.Capacity"/>)
+        /// </param>
         [UnityLogListener.Exclude]
         public void Error(in FixedString64Bytes message)
         {
@@ -71,6 +146,16 @@ namespace Anvil.Unity.DOTS.Logging
             UnityEngine.Debug.LogError($"{MessagePrefix}{message}");
         }
 
+        /// <summary>
+        /// Logs an error message.
+        /// </summary>
+        /// <typeparam name="T">
+        /// The FixedString type for the message
+        /// Select the shortest fixed string that will fit the message length.
+        /// </typeparam>
+        /// <param name="message">
+        /// The message to log. (max length: <see cref="T.Capacity"/>)
+        /// </param>
         [UnityLogListener.Exclude]
         public void Error<T>(in T message) where T : struct, INativeList<byte>, IUTF8Bytes
         {
--- Scripts/Runtime/Logging/LoggerExtension.cs
+++ Scripts/Runtime/Logging/LoggerExtension.cs
@@ -1,16 +1,42 @@
 using Anvil.CSharp.Logging;
-using Anvil.Unity.DOTS.Logging;
 using Unity.Collections;
 
-public static class LoggerExtension
+namespace Anvil.Unity.DOTS.Logging
 {
-    public static BurstableLogger<FixedString32Bytes> AsBurstable(this in Log.Logger logger, string appendToMessagePrefix = null)
+    /// <summary>
+    /// A collection of DOTS specific extension methods for <see cref="Log.Logger"/>.
+    /// </summary>
+    public static class LoggerExtension
     {
-        return AsBurstable<FixedString32Bytes>(logger, appendToMessagePrefix);
-    }
+        /// <summary>
+        /// Creates an instance of <see cref="BurstableLogger{PrefixStringType}"/> from the
+        /// <see cref="Log.Logger" /> instance.
+        /// </summary>
+        /// <param name="logger">The <see cref="Log.Logger" /> to copy configuration from.</param>
+        /// <param name="appendToMessagePrefix">
+        /// A string to append to the <see cref="Log.Logger" />'s existing prefix.
+        /// (max length: <see cref="FixedString32Bytes.Capacity"/>)
+        /// </param>
+        /// <returns>A <see cref="BurstableLogger{FixedString32Bytes}" /> instance.</returns>
+        public static BurstableLogger<FixedString32Bytes> AsBurstable(this in Log.Logger logger, string appendToMessagePrefix = null)
+        {
+            return AsBurstable<FixedString32Bytes>(logger, appendToMessagePrefix);
+        }
 
-    public static BurstableLogger<PrefixStringType> AsBurstable<PrefixStringType>(this in Log.Logger logger, string appendToMessagePrefix = null) where PrefixStringType : struct, INativeList<byte>, IUTF8Bytes
-    {
-        return new BurstableLogger<PrefixStringType>(logger, appendToMessagePrefix);
+        /// <summary>
+        /// Creates an instance of <see cref="BurstableLogger{PrefixStringType}"/> from the
+        /// <see cref="Log.Logger" /> instance.
+        /// </summary>
+        /// <typeparam name="PrefixStringType"></typeparam>
+        /// <param name="logger">The <see cref="Log.Logger" /> to copy configuration from.</param>
+        /// <param name="appendToMessagePrefix">
+        /// A string to append to the <see cref="Log.Logger" />'s existing prefix.
+        /// (max length: <see cref="PrefixStringType.Capacity"/>)
+        /// </param>
+        /// <returns>A <see cref="BurstableLogger{PrefixStringType}" /> instance.</returns>
+        public static BurstableLogger<PrefixStringType> AsBurstable<PrefixStringType>(this in Log.Logger logger, string appendToMessagePrefix = null) where PrefixStringType : struct, INativeList<byte>, IUTF8Bytes
+        {
+            return new BurstableLogger<PrefixStringType>(logger, appendToMessagePrefix);
+        }
     }
 }
