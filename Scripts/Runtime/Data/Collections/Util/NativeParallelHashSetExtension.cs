4af1e607087ae4656bcb3ca67d9e8a6531f3b1d1
Mike Baker <1426795+mbaker3@users.noreply.github.com>
2022-11-11 12:01:44 -0500

    Implement NativeParallelhashSetExtension.CopyFrom

---
+++ Scripts/Runtime/Data/Collections/Util/NativeParallelHashSetExtension.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Collections.Generic;
+using Unity.Burst;
+using Unity.Collections;
+
+namespace Anvil.Unity.DOTS.Data
+{
+    /// <summary>
+    /// A collection of extension methods for <see cref="NativeParallelHashSet{T}"/>.
+    /// </summary>
+    public static class NativeParallelHashSetExtension
+    {
+        /// <summary>
+        /// Copy the contents of one <see cref="NativeParallelHashSet{T}"/> to another overwriting any existing data.
+        /// </summary>
+        /// <param name="destination">The <see cref="NativeParallelHashSet{T}"/> to copy values into.</param>
+        /// <param name="source">The <see cref="NativeParallelHashSet{T}"/> to copy values from.</param>
+        /// <typeparam name="T">The element type of the collection.</typeparam>
+        [BurstCompile]
+        public static void CopyFrom<T>(this NativeParallelHashSet<T> destination, NativeParallelHashSet<T> source)
+            where T : unmanaged, IEquatable<T>
+        {
+            int sourceCount = source.Count();
+            if (destination.Capacity < sourceCount)
+            {
+                destination.Capacity = sourceCount;
+            }
+
+            destination.UnionWith(source);
+        }
+
+
+        /// <summary>
+        /// Copy the contents of an <see cref="IEnumerable{T}"/> into a <see cref="NativeParallelHashSet{T}"/>
+        /// overwriting any existing data.
+        /// </summary>
+        /// <param name="destination">The <see cref="NativeParallelHashSet{T}"/> to copy values into.</param>
+        /// <param name="source">The <see cref="IEnumerable{T}"/> to copy values from.</param>
+        /// <typeparam name="T">The element type of the collection</typeparam>
+        /// <remarks>
+        /// Attempts to pre-allocate the capacity in the destination if the source's count can be determined ahead of
+        /// time.
+        /// </remarks>
+        [BurstCompile]
+        public static void CopyFrom<T>(this NativeParallelHashSet<T> destination, IEnumerable<T> source)
+            where T : unmanaged, IEquatable<T>
+        {
+            int? sourceCount = (source as ICollection<T>)?.Count ?? (source as IReadOnlyCollection<T>)?.Count;
+
+            if (sourceCount.HasValue && destination.Capacity < sourceCount)
+            {
+                destination.Capacity = sourceCount.Value;
+            }
+
+            destination.Clear();
+
+            foreach (T val in source)
+            {
+                destination.Add(val);
+            }
+        }
+    }
+}
