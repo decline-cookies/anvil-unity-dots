e6a27eb4c1aaefa3222ae40e6b9aa19c892f6e06
Mike Baker <1426795+mbaker3@users.noreply.github.com>
2022-12-01 21:52:59 -0500

    MathExtension - Move non DOTS extensions to anvil-unity-core
    IsApproximately, GetInverse(Safe), ToSignedInfinite

--- Scripts/Runtime/Mathematics/MathExtension.cs
+++ Scripts/Runtime/Mathematics/MathExtension.cs
@@ -11,33 +11,6 @@ namespace Anvil.Unity.DOTS.Mathematics
     public static class MathExtension
     {
         /// <summary>
-        /// Get the inverse of a <see cref="float3"/>.
-        /// </summary>
-        /// <remarks>
-        /// Any components that are 0 will invert to <see cref="float.NaN"/>.
-        /// Use <see cref="GetInverseSafe" /> if this is a possibility.
-        /// </remarks>
-        /// <param name="value">The value to get the inverse of.</param>
-        /// <returns>The inverted value</returns>
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static float3 GetInverse(this float3 value)
-        {
-            return 1f / value;
-        }
-
-        /// <summary>
-        /// Get the inverse of a <see cref="float3"/> with any <see cref="float.NaN"/> components set to 0
-        /// </summary>
-        /// <param name="value">The value to get the inverse of.</param>
-        /// <returns>The inverted value</returns>
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static float3 GetInverseSafe(this float3 value)
-        {
-            float3 inverse = value.GetInverse();
-            return math.select(inverse, float3.zero, math.isnan(inverse));
-        }
-
-        /// <summary>
         /// Get the scale component from a TRS matrix.
         /// On a LocalToWorld matrix this is the world scale.
         /// </summary>
@@ -97,64 +70,6 @@ namespace Anvil.Unity.DOTS.Mathematics
         public static float3 GetPosition(this float4x4 matrix)
         {
             return new float3(matrix.c0.w, matrix.c1.w, matrix.c2.w);
-        }
-
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static bool IsApproximately(this float a, float b)
-        {
-            return math.abs(a - b) < MathUtil.FLOATING_POINT_EQUALITY_TOLERANCE;
-        }
-
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static bool2 IsApproximately(this float2 a, float2 b)
-        {
-            return math.abs(a - b) < MathUtil.FLOATING_POINT_EQUALITY_TOLERANCE;
-        }
-
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static bool3 IsApproximately(this float3 a, float3 b)
-        {
-            return math.abs(a - b) < MathUtil.FLOATING_POINT_EQUALITY_TOLERANCE;
-        }
-
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static bool4 IsApproximately(this float4 a, float4 b)
-        {
-            return math.abs(a - b) < MathUtil.FLOATING_POINT_EQUALITY_TOLERANCE;
-        }
-
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static bool3x3 IsApproximately(this float3x3 a, float3x3 b)
-        {
-            return new bool3x3(
-                a.c0.IsApproximately(b.c0),
-                a.c1.IsApproximately(b.c1),
-                a.c2.IsApproximately(b.c2)
-            );
-        }
-
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static bool4x4 IsApproximately(this float4x4 a, float4x4 b)
-        {
-            return new bool4x4(
-                a.c0.IsApproximately(b.c0),
-                a.c1.IsApproximately(b.c1),
-                a.c2.IsApproximately(b.c2),
-                a.c3.IsApproximately(b.c3)
-            );
-        }
-
-        /// <summary>
-        /// Converts the components of a value to infinite quantities where:
-        /// >0 = <see cref="float.PositiveInfinity"/>
-        /// <0 = <see cref="float.NegativeInfinity"/>
-        /// 0 = 0
-        /// </summary>
-        /// <param name="value">The value to convert.</param>
-        /// <returns>A signed infinite value.</returns>
-        public static float3 ToSignedInfinite(this float3 value)
-        {
-            return math.select(math.sign(value) * new float3(float.PositiveInfinity), value, value == 0);
         }
 
         /// <summary>
