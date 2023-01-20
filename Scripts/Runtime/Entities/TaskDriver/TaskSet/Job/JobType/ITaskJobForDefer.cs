9e808677a145d2b57c670cf7ccae107686e3a486
Jon Keon <jon.keon@gmail.com>
2023-01-20 08:54:33 -0500

    Adding TaskJobForDefer

---
+++ Scripts/Runtime/Entities/TaskDriver/TaskSet/Job/JobType/ITaskJobForDefer.cs
@@ -0,0 +1,30 @@
+using Anvil.Unity.DOTS.Data;
+using Unity.Jobs;
+using Unity.Jobs.LowLevel.Unsafe;
+
+namespace Anvil.Unity.DOTS.Entities.Tasks
+{
+    /// <summary>
+    /// A replacement for <see cref="IJobFor"/> when the number of work items is not known at Schedule time
+    /// and you are using a <see cref="DeferredNativeArray{T}"/>
+    /// This is specific to a context where the data is being updated.
+    /// </summary>
+    /// <typeparam name="TInstance">The type of <see cref="IEntityProxyInstance"/> data</typeparam>
+    [JobProducerType(typeof(TaskJobForDeferExtension.WrapperJobStruct<,>))]
+    public interface ITaskJobForDefer<in TInstance>
+        where TInstance : unmanaged, IEntityProxyInstance
+    {
+        /// <summary>
+        /// Called once per thread to allow for initialization of state in the job
+        /// </summary>
+        /// <param name="nativeThreadIndex">The native thread index that the job is running on</param>
+        void InitForThread(int nativeThreadIndex);
+
+        /// <summary>
+        /// This method is called for each instance that is being read, allowing for the work to
+        /// occur.
+        /// </summary>
+        /// <param name="instance">The <see cref="IEntityProxyInstance"/> to read.</param>
+        void Execute(TInstance instance);
+    }
+}
