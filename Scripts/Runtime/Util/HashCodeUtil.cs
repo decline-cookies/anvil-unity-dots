e286531e76678a786f089188d196dc04440dcc41
Mike Baker <1426795+mbaker3@users.noreply.github.com>
2023-07-19 16:20:43 -0400

    HashCodeUtil - Fix Docs, Add overloads for more params

--- Scripts/Runtime/Util/HashCodeUtil.cs
+++ Scripts/Runtime/Util/HashCodeUtil.cs
@@ -1,19 +1,24 @@
+using System.Runtime.CompilerServices;
+using Unity.Burst;
+
 namespace Anvil.Unity.DOTS.Util
 {
     /// <summary>
-    /// Helper class for generating hashcodes that are Burst compatible.
+    /// Helper class for generating hash codes that is Burst compatible.
     /// </summary>
+    [BurstCompile]
     public static class HashCodeUtil
     {
         /// <summary>
-        /// Returns a HashCode that combines two initial hash codes.
+        /// Returns a hash code that combines two initial hash codes.
         /// </summary>
         /// <param name="h1">The first hash code</param>
         /// <param name="h2">The second hash code</param>
-        /// <returns></returns>
+        /// <returns>A combined hash code</returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static int GetHashCode(int h1, int h2)
         {
-            //Taken from ValueTuple.cs GetHashCode. 
+            //Taken from ValueTuple.cs GetHashCode.
             //https://github.com/dotnet/roslyn/blob/main/src/Compilers/Test/Resources/Core/NetFX/ValueTuple/ValueTuple.cs
             //Licence is a-ok as per the top of the linked file:
             // - Licensed to the .NET Foundation under one or more agreements.
@@ -22,6 +27,37 @@ namespace Anvil.Unity.DOTS.Util
             //Unfortunately we can't use directly because it has a static Random class it creates which doesn't jive with Burst
             uint rol5 = ((uint)h1 << 5) | ((uint)h1 >> 27);
             return ((int)rol5 + h1) ^ h2;
+        }
+
+        /// <summary>
+        /// Returns a hash code that combines three initial hash codes.
+        /// </summary>
+        /// <param name="h1">The first hash code</param>
+        /// <param name="h2">The second hash code</param>
+        /// <param name="h3">The third hash code</param>
+        /// <returns>A combined hash code</returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static int GetHashCode(int h1, int h2, int h3)
+        {
+            return GetHashCode(
+                GetHashCode(h1, h2),
+                h3);
+        }
+
+        /// <summary>
+        /// Returns a hash code that combines four initial hash codes.
+        /// </summary>
+        /// <param name="h1">The first hash code</param>
+        /// <param name="h2">The second hash code</param>
+        /// <param name="h3">The third hash code</param>
+        /// <param name="h4">The fourth hash code</param>
+        /// <returns>A combined hash code</returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static int GetHashCode(int h1, int h2, int h3, int h4)
+        {
+            return GetHashCode(
+                GetHashCode(h1, h2),
+                GetHashCode(h3, h4));
         }
     }
 }
