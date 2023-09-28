450aea90bbefce836468d1d5b522884f3e7b17dd
Mike Baker <1426795+mbaker3@users.noreply.github.com>
2023-09-28 11:08:54 -0400

    IWorldUniqueID - Improve docs

--- Scripts/Runtime/Entities/Data/IWorldUniqueID.cs
+++ Scripts/Runtime/Entities/Data/IWorldUniqueID.cs
@@ -4,16 +4,14 @@ namespace Anvil.Unity.DOTS.Entities
 {
     /// <summary>
     /// Provides a <see cref="World"/> unique ID.
-    /// This ID is unique within the current World instance.
-    /// This ID is not unique globally between all world instances.
+    /// This ID is unique within the current World instance and guaranteed to be the same across all World instances.
     /// </summary>
     /// <typeparam name="TWorldUniqueID">The type of ID</typeparam>
     public interface IWorldUniqueID<out TWorldUniqueID>
     {
         /// <summary>
         /// The <see cref="World"/> unique ID.
-        /// This ID is unique within the current World instance.
-        /// This ID is not unique globally between all world instances.
+        /// This ID is unique within the current World instance and guaranteed to be the same across all World instances.
         /// </summary>
         public TWorldUniqueID WorldUniqueID { get; }
     }
