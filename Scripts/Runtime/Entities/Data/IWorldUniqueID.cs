8d42f49be63844d801f836895cb949507022eabf
Mike Baker <1426795+mbaker3@users.noreply.github.com>
2023-09-28 10:38:44 -0400

    IWorldUniqueID - Improve docs
    To clarify the meaning of "World Unique"

--- Scripts/Runtime/Entities/Data/IWorldUniqueID.cs
+++ Scripts/Runtime/Entities/Data/IWorldUniqueID.cs
@@ -3,13 +3,17 @@ using Unity.Entities;
 namespace Anvil.Unity.DOTS.Entities
 {
     /// <summary>
-    /// Provides a <see cref="World"/> unique ID
+    /// Provides a <see cref="World"/> unique ID.
+    /// This ID is unique within the current World instance.
+    /// This ID is not unique globally between all world instances.
     /// </summary>
     /// <typeparam name="TWorldUniqueID">The type of ID</typeparam>
     public interface IWorldUniqueID<out TWorldUniqueID>
     {
         /// <summary>
-        /// The <see cref="World"/> unique ID
+        /// The <see cref="World"/> unique ID.
+        /// This ID is unique within the current World instance.
+        /// This ID is not unique globally between all world instances.
         /// </summary>
         public TWorldUniqueID WorldUniqueID { get; }
     }
