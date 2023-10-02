eb93f9422047e2ac94e4d67d62ac69fe3d51981b
Mike Baker <1426795+mbaker3@users.noreply.github.com>
2023-10-02 15:11:08 -0400

    AbstractDataSystem - Add to a default group
    As a temporary measure, until #172 is worked on, `AbstractDataSystem` is now added to the group `AbstractDataSystem.Group` in the `InitializationSystemGroup`.
    
    This keeps all of the data systems together in the player loop until we have a chance to remove them.

--- Scripts/Runtime/Entities/AbstractDataSystem.cs
+++ Scripts/Runtime/Entities/AbstractDataSystem.cs
@@ -1,11 +1,18 @@
+using Unity.Entities;
+
 namespace Anvil.Unity.DOTS.Entities
 {
     /// <summary>
     /// A system that exists to take advantage of the lifecycle of a world but does not actually need to update.
     /// Used for lookups to get/store data.
     /// </summary>
+    [UpdateInGroup(typeof(Group))]
     public abstract partial class AbstractDataSystem : AbstractAnvilSystemBase
     {
+        //TODO: #172 - Temp to hold all AbstractDataSystem classes until we remove them from the PlayerLoop
+        [UpdateInGroup(typeof(InitializationSystemGroup))]
+        private partial class Group : ComponentSystemGroup { }
+
         //TODO: #172 - Remove from PlayerLoop
         protected override void OnCreate()
         {
